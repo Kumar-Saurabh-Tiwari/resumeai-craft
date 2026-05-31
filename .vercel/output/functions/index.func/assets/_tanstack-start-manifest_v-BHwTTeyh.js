447cfe11432757b5bfb055abf4a3ee59a58d0bfa
Kumar-Saurabh-Tiwari <tiwarisaurabhkumar0@gmail.com>
2026-05-31 15:43:48 +0530

    changes updated

--- .vercel/output/functions/index.func/assets/_tanstack-start-manifest_v-BHwTTeyh.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/resumeai-craft/src/routes/__root.tsx", children: ["/", "/builder"], preloads: ["/assets/index-JWSXOQEe.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-JWSXOQEe.js" } }] }, "/": { filePath: "D:/resumeai-craft/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-B93yCsjB.js", "/assets/ResumePreview-D7NpqbE5.js"] }, "/builder": { filePath: "D:/resumeai-craft/src/routes/builder.tsx", children: void 0, preloads: ["/assets/builder-Mh1GiRqR.js", "/assets/ResumePreview-D7NpqbE5.js"] } } });
-export {
-  tsrStartManifest
-};
